3beaee99df81376f93eee7bd6fc248ffd1d58b78
zzc <zhaoz1193@gmail.com>
2021-09-16 10:54:50 +0800

    part03String

--- leptjson.c
+++ leptjson.c
@@ -1,12 +1,18 @@
 #include "leptjson.h"
 #include <assert.h> /*assert()*/
-#include <stdlib.h> /*NULL*/
+#include <stdlib.h> /* NULL  malloc realloc*/
 #include <errno.h> /*errno*/
 #include <math.h> /* HUGE_VAL */
+#include <string.h> /* memcpy() */
 
-#define EXPECT(c, ch) do { assert(*c->json == (ch)); c->json++;} while(0)
-#define ISDIGIT1TO9(ch) ((ch) >= '1' && (ch) <= '9')
-#define ISDIGIT(ch) ((ch) >= '0' && (ch) <= '9')
+#ifndef LEPT_PARSE_STACK_INIT_SIZE
+#define LEPT_PARSE_STACK_INIT_SIZE 256
+#endif
+
+#define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++;} while(0)
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define PUTC(c, ch)         do { *(char*)lept_context_push(c, sizeof(char)) = (ch); } while(0)
 
 
 /*
@@ -15,6 +21,8 @@
 typedef struct 
 {
     const char* json;
+    char* stack; /* 动态解析字符串的临时缓冲区，因为是先进后出，所以才用栈的结构 */
+    size_t size,top; /* size为当前堆栈的容量，top是栈顶的位置 ，扩展stack,不要把top用指针形式存储 */
 }lept_context;
 
 /*
@@ -26,6 +34,33 @@ f ➔ false
 [ ➔ array
 { ➔ object
 */
+
+/* 向栈里面push元素 ；
+这个堆栈是以字节储存的。每次可要求压入任意大小的数据，它会返回数据起始的指针*/
+static void* lept_context_push(lept_context* c,size_t size){
+    void* ret;
+    assert(size > 0);
+    if(c->top + size >= c->size){
+        /* 如果为零，先初始化 */
+        if(c->size == 0) {
+            c->size = LEPT_PARSE_STACK_INIT_SIZE;
+        }
+        while (c->top + size > c->size)
+        {
+            c->size += c->size >> 1; /* 1.5倍容量扩容 */
+        }
+        c->stack = (char*)realloc(c->stack ,c->size); /* realloc重新分配内存，不用为第一次内存分配特殊处理 */
+    }
+    ret = c->stack + c->size; /* 将指针前移size大小 */
+    c->top += c->size; /* top栈顶指针移动size大小 */
+    return ret;
+}
+
+
+static void* lept_context_pop(lept_context* c,size_t size){
+    assert(c->top >= size); /* 确保有空间可以弹出 */
+    return c->stack + (c->top -= size); /* c->stack 加上栈顶指针往下移size大小 */
+}
 
 static void lept_parse_whitespace(lept_context* c){
     const char *p = c->json;
@@ -116,9 +151,9 @@ static int lept_parse_number(lept_context* c,lept_value* v){
         for(p++ ;ISDIGIT(*p); p++);
     }
     errno =  0;
-    v->num = strtod(c->json ,NULL);
+    v->u.num = strtod(c->json ,NULL);
     /* 判断数字是否过大 */
-    if(errno == ERANGE && (v->num == HUGE_VAL || v->num == -HUGE_VAL)){
+    if(errno == ERANGE && (v->u.num == HUGE_VAL || v->u.num == -HUGE_VAL)){
         return LEPT_PARSE_NUMBER_TOO_BIG;
     }
     v->type = LEPT_NUMBER;
@@ -126,6 +161,59 @@ static int lept_parse_number(lept_context* c,lept_value* v){
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_string(lept_context* c,lept_value *v){
+    size_t head = c->top,len; /* 备份栈顶 */
+    const char* p;
+    EXPECT(c,'\"');/* 字符串是以"开始的 */
+    p = c->json;
+    for(; ;){
+        char ch = *p++;
+        switch (ch)
+        {
+        case '\"':
+            len = c->top - head ; /* 字符入栈之后栈顶指针会增加，减去原来的栈顶，得到字符串的长度 */
+            lept_set_string(v,(const char*)lept_context_pop(c,len),len); /* 将字符串 c拷贝到字符串v里面*/
+            c->json = p; /*  */
+            return LEPT_PARSE_OK;
+        case '\0':
+            c->top = head;
+            return LEPT_PARSE_MISS_QUOTATION_MARK;
+        default:
+            PUTC(c, ch); /* 将字符入栈 */
+            break;
+        }
+    }
+}
+
+void lept_free(lept_value* v){
+    assert(v != NULL);
+    if(v->type == LEPT_STRING){
+        free(v->u.str.s);
+    }
+    v->type = LEPT_NULL;
+}
+
+/* 将s 拷贝到v.u.str中去  */
+void lept_set_string(lept_value* v,const char* s,size_t len){
+    assert(v != NULL && (s != NULL || len == 0));
+    lept_free(v);
+    v->u.str.s = (char*)malloc(len + 1);
+    memcpy(v->u.str.s ,s ,len);
+    v->u.str.s[len] = '\0';
+    v->u.str.len = len ;
+    v->type = LEPT_STRING;
+}
+
+const char* lept_get_string(const lept_value* v){
+    assert(v != NULL && v->type == LEPT_STRING);
+    return v->u.str.s;
+}
+
+/* 获取字符串的长度 */
+size_t lept_get_string_length(const lept_value* v){
+    assert(v != NULL && v->type == LEPT_STRING);
+    return v->u.str.len;
+}
 
 static int lept_parse_value(lept_context* c ,lept_value* v){
     switch (*c->json)
@@ -142,6 +230,9 @@ static int lept_parse_value(lept_context* c ,lept_value* v){
     case '\0':
         return LEPT_PARSE_EXPECT_VALUE;
         break;
+    case '"':
+        return lept_parse_string(c,v);
+        break;
     default:
         return lept_parse_number(c,v);
         break;
@@ -153,7 +244,9 @@ int lept_parse(lept_value* v,const char* json){
     int ret ;
     assert(v != NULL);
     c.json = json;
-    v->type = LEPT_NULL;
+    c.stack = NULL;
+    c.size = c.top = 0;
+    lept_init(v);
     lept_parse_whitespace(&c);
     ret = lept_parse_value(&c,v);
     if(ret == LEPT_PARSE_OK){
@@ -163,6 +256,8 @@ int lept_parse(lept_value* v,const char* json){
             ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
         }
     }
+    assert(c.top == 0) ;/* 确保所有元素均弹出栈*/
+    free(c.stack);
     return ret;
 }
 
@@ -171,7 +266,26 @@ lept_type lept_get_type(const lept_value* v){
     return v->type;
 }
 
+/* 获取v中num的值 */
 double lept_get_number(const  lept_value *v){
     assert(v !=NULL && v->type == LEPT_NUMBER);
-    return v->num;
+    return v->u.num;
+}
+
+/* 将数字num写入v中 */
+void lept_set_number(lept_value* v, double num){
+    lept_free(v);
+    v->u.num = num;
+    v->type = LEPT_NUMBER;
+}
+
+int lept_get_boolean(const lept_value* v){
+    assert(v != NULL && (v->type == LEPT_TRUE || v->type == LEPT_FALSE));
+    return v->type == LEPT_TRUE;
+}
+
+/* 将b写入v中 */
+void lept_set_boolean(lept_value *v ,int b){
+    lept_free(v);
+    v->type = b ? LEPT_TRUE : LEPT_FALSE; /* b为1设置为true,0为false */
 }
--- leptjson.h
+++ leptjson.h
@@ -1,13 +1,33 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
+#include <stddef.h> /* size_t */
+
 /*json中有6中数据类型，声明一个枚举类型*/
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
 
 /*需要实现一个树的数据结构，每个节点使用 lept_value 结构体表示，我们会称它为一个 JSON 值（JSON value）*/
+/*64位系统 32字节 */
+#if 0
 typedef struct {
+    char* s;
+    size_t len;
     double num; /*数字的表示方式，采用双精度浮点数*/
+    lept_type type;
+}lept_value2;
+#endif
+
+/* 设置为联合体是因为节省内存 24字节 */
+typedef struct{
+    union 
+    {
+        struct{
+            char *s;
+            size_t len;
+        }str;       /* string ,字符串以及长度 */
+        double num; /* 数字 number */
+    }u;
     lept_type type;
 }lept_value;
 
@@ -17,8 +37,13 @@ enum {
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
     LEPT_PARSE_ROOT_NOT_SINGULAR,
-    LEPT_PARSE_NUMBER_TOO_BIG
+    LEPT_PARSE_NUMBER_TOO_BIG,
+    LEPT_PARSE_MISS_QUOTATION_MARK,
+    LEPT_PARSE_INVALID_STRING_ESCAPE,
+    LEPT_PARSE_INVALID_STRING_CHAR
 };
+
+#define lept_init(v) do{ (v)->type = LEPT_NULL;} while(0)
 
 /*解析json ,
 传入参数 json:传入的 JSON 文本是一个 C 字符串（空结尾字符串／null-terminated string），
@@ -29,17 +54,25 @@ enum {
 lept_value v;
 const char json[] = ...;
 int ret = lept_parse(&v,json)*/
-
 int lept_parse(lept_value* v, const char* json);
 
-/*
-现时我们只需要一个访问结果的函数，就是获取其类型
-*/
+/*现时我们只需要一个访问结果的函数，就是获取其类型*/
 lept_type lept_get_type(const lept_value* v);
 
-/* 
-获取json中的数字
- */
+
+/* 获取json中的数字*/
 double lept_get_number(const lept_value* v);
+void lept_set_number(lept_value* v ,double num);
+
+void lept_free(lept_value* v);
+#define lept_set_null(v) lept_free(v);
+
+
+int lept_get_boolean(const lept_value* v);
+void lept_set_boolean(lept_value*v ,int b);
+
+const char* lept_get_string(const lept_value* v);
+size_t lept_get_string_length(const lept_value* v);
+void lept_set_string(lept_value* v,const char* s,size_t len);
 
 #endif /* LEPTJSON_H__ */
